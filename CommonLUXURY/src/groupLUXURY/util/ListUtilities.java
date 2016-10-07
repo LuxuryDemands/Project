b7dbc5ae26b045b6bc241f6f313988edfb2d90b3
Sebastian <Sebastian@Sebastian-PC>
2016-10-07 13:02:26 -0400

    started sort method in ListUtilities.java | Part 2 Phase 2

--- CommonLUXURY/src/groupLUXURY/util/ListUtilities.java
+++ CommonLUXURY/src/groupLUXURY/util/ListUtilities.java
@@ -8,5 +8,24 @@ package groupLUXURY.util;
  *
  */
 public class ListUtilities {
+	
+	@SuppressWarnings({"rawtypes","unchecked"})
+	public static void sort(Comparable[] list) {
+		int length = list.length;
+		int j, first, temp;
+		for (int i = 0; ) {
+			first = 0; // initialize to subscript of first element
+			for (j = 1; j <= i; j++) // locate smallest element between
+										// positions 1 and i.
+			{
+				if (num[j] < num[first])
+					first = j;
+			}
+			temp = num[first]; // swap smallest found with element in position
+								// i.
+			num[first] = num[i];
+			num[i] = temp;
+		}
+	}
 
 }
