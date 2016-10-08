c8240dc1dd7ae288e5581ce682c31a95d2e52e00
Sebastian <Sebastian@Sebastian-PC>
2016-10-08 13:01:55 -0400

    new test class for ListUtilities

---
+++ CommonLUXURY/test/group/util/ListUtilitiesTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package group.util;
+
+/**
+ * @author Sebastian
+ *
+ */
+public class ListUtilitiesTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
