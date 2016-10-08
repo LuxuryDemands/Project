b6269b43be8493275ede74e50ed97e8bfa2c8dc0
Sebastian <Sebastian@Sebastian-PC>
2016-10-07 20:40:03 -0400

    rewrote sort code to make it sequential

--- CommonLUXURY/src/groupLUXURY/util/ListUtilities.java
+++ CommonLUXURY/src/groupLUXURY/util/ListUtilities.java
@@ -13,6 +13,16 @@ public class ListUtilities {
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void sort(Comparable[] list) throws IllegalArgumentException,NullPointerException {
-		Arrays.sort(list);
-	}
+		for (int i = 0; i < list.length - 1; i++)
+        {
+            int index = i;
+            for (int j = i + 1; j < list.length; j++)
+                if (list[j].compareTo(list[index])>0) 
+                    index = j;
+      
+            Comparable smallest = list[index];  
+            list[index] = list[i];
+            list[i] = smallest;
+        }
+    }
 }
