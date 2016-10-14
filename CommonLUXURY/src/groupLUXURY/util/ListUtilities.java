e9ace4aefe63a8fbb04c19e50e2389b7d368e501
1331680 <1331680@2F14-18.ad.dawsoncollege.qc.ca>
2016-10-14 17:32:35 -0400

    Merge method almost complete, added test case to hotelfileloader

--- CommonLUXURY/src/groupLUXURY/util/ListUtilities.java
+++ CommonLUXURY/src/groupLUXURY/util/ListUtilities.java
@@ -43,64 +43,83 @@ public class ListUtilities {
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static Comparable[] merge(Comparable[] list1, Comparable[] list2) {
-		if (list1==null || list2 == null){
+		if (list1 == null || list2 == null) {
 			throw new NullPointerException("Neither of the lists can be null");
 		}
-		if (checkNull(list1) || checkNull(list2)){
+		if (checkNull(list1) || checkNull(list2)) {
 			throw new IllegalArgumentException("One of the lists is not full to capacity");
 		}
-		Comparable[] list3 = (Comparable[]) Array.newInstance(list1.getClass().getComponentType(),list1.length + list2.length);
-		int i1 =0, j1=0;
+		Comparable[] list3 = (Comparable[]) Array.newInstance(list1.getClass().getComponentType(),
+				list1.length + list2.length);
+		int i1 = 0, j1 = 0;
 		while (i1 < list1.length) {
 
-				list3[j1] = list1[i1];
-				j1++;
-				list3[j1] = list2[i1];
-				j1++;
-				i1++;
+			list3[j1] = list1[i1];
+			j1++;
+			list3[j1] = list2[i1];
+			j1++;
+			i1++;
 		}
 		sort(list3);
-		//int p=0;
-		//int q=0;
-		//int r=0;
-		//
-		//make new array 
-		//Comparable[] list3 = (Comparable[]) Array.newInstance(list1.getClass().getComponentType(),
-				//list1.length + list2.length);
-		
-//		while (p<list1.length && q<list2.length)
-//		{
-//			if (list1[p].compareTo(list2[q]) == 0 ) // if same
-//			{
-//				//save p
-//				list3[r++] = list1[p];
-//				p++;
-//				q++;
-//				System.out.println("duplicate found:" + list3[r]);
-//			}
-//			else if (list1[p].compareTo(list2[q]) < 0 ) //p is smaller
-//			{
-//				//save p
-//				list3[r++] = list1[p];
-//				p++;
-//			}
-//			else if (list1[p].compareTo(list2[q]) > 0 ) //q is smaller
-//			{
-//				//save q
-//				list3[r++] = list2[q];
-//				q++;
-//			}
-//		}
-//		
-//		//shrink array by using the total counter r
-//		Comparable[] listToReturn = Arrays.copyOf(list3, r);
-			
-
-		
-		
-		Comparable [] listToReturn = list3;
+		for(int index = 0; index<list3.length;index++){
+			System.out.println(list3[index]);
+		}
+		System.out.println("=======SEPARATOR=======");
+		Comparable[] duplicates = arrayDuplicates(list3);
+		Comparable[] list4 = (Comparable[]) Array.newInstance(list3.getClass().getComponentType(),
+				list3.length - duplicates.length);
+		for (int j2 = 0,i2=0; j2 < list3.length; j2++,i2++){
+			for (int k2 = 0; k2 < duplicates.length; k2++) {
+				if (list3[j2].compareTo(duplicates[k2]) == 0) {
+					list4[i2] = list3[j2];
+					j2++;
+				} else {
+					list4[i2] = list3[j2];
+				}
+			}
+		}
+		// }
+		Comparable[] listToReturn = list4;
 		return listToReturn;
 	}
+
+	// isaak code vvvvv
+	// int p=0;
+	// int q=0;
+	// int r=0;
+	//
+	// make new array
+	// Comparable[] list3 = (Comparable[])
+	// Array.newInstance(list1.getClass().getComponentType(),
+	// list1.length + list2.length);
+
+	// while (p<list1.length && q<list2.length)
+	// {
+	// if (list1[p].compareTo(list2[q]) == 0 ) // if same
+	// {
+	// //save p
+	// list3[r++] = list1[p];
+	// p++;
+	// q++;
+	// System.out.println("duplicate found:" + list3[r]);
+	// }
+	// else if (list1[p].compareTo(list2[q]) < 0 ) //p is smaller
+	// {
+	// //save p
+	// list3[r++] = list1[p];
+	// p++;
+	// }
+	// else if (list1[p].compareTo(list2[q]) > 0 ) //q is smaller
+	// {
+	// //save q
+	// list3[r++] = list2[q];
+	// q++;
+	// }
+	// }
+	//
+	// //shrink array by using the total counter r
+	// Comparable[] listToReturn = Arrays.copyOf(list3, r);
+	// end isaak code^^^
 
 	private static int count(String item) {
 		return item.split("[*]").length;
@@ -117,6 +136,7 @@ public class ListUtilities {
 		return false;
 	}
 
+	@SuppressWarnings("rawtypes")
 	public static int countDuplicates(Comparable[] list) {
 		int count = 0;
 		for (int i = 0; i < list.length; i++) {
@@ -128,5 +148,22 @@ public class ListUtilities {
 			}
 		}
 		return count;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public static Comparable[] arrayDuplicates(Comparable[] list) {
+		Comparable[] duplicateList = (Comparable[]) Array.newInstance(list.getClass().getComponentType(),
+				countDuplicates(list));
+		int count = 0;
+		for (int i = 0; i < list.length; i++) {
+			if (i == list.length - 1) {
+				break;
+			}
+			if (list[i].equals(list[i + 1])) {
+				duplicateList[count] = list[i];
+				count++;
+			}
+		}
+		return duplicateList;
 	}
 }
