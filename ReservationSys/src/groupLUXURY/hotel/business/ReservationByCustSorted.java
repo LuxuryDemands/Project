65eb114cbaa111ed93b381752a3be304b27501e2
kajal <kajal@192.168.1.11>
2016-10-25 21:04:02 -0400

    Started ReservationByCustSorted class

--- ReservationSys/src/groupLUXURY/hotel/business/ReservationByCustSorted.java
+++ ReservationSys/src/groupLUXURY/hotel/business/ReservationByCustSorted.java
@@ -1,20 +1,20 @@
-/**
- * 
- */
 package groupLUXURY.hotel.business;
-
+import java.util.Comparator;
+import dw317.hotel.business.interfaces.Reservation;
 /**
  * @author kajal
  *
  */
-public class ReservationByCustSorted {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
-
+public class ReservationByCustSorted implements Comparator<Reservation> {
+@Override
+public int compare(Reservation r1, Reservation r2) {
+if (r1.equals(r2))
+return 0;
+if (!r1.getCustomer().equals(r2.getCustomer()))
+return r1.getCustomer().compareTo(r2.getCustomer());
+//if same customer, order by check in (i.e., default)
+return r1.compareTo(r2);
+}
 }
