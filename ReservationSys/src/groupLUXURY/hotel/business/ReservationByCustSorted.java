6db2c57901eff20523a3558b997908e543658035
kajal <kajal@192.168.1.11>
2016-10-25 21:03:31 -0400

    Starting the ReservationByCustSorted class

---
+++ ReservationSys/src/groupLUXURY/hotel/business/ReservationByCustSorted.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package groupLUXURY.hotel.business;
+
+/**
+ * @author kajal
+ *
+ */
+public class ReservationByCustSorted {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
