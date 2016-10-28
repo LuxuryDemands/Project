a0af4d32bc7c95c08b3be476e60a0b504643a7d1
kajal <kajal@192.168.1.11>
2016-10-28 19:43:35 -0400

    Bug fix

--- ReservationSys/src/groupLUXURY/hotel/business/ReservationByCheckOutSorted.java
+++ ReservationSys/src/groupLUXURY/hotel/business/ReservationByCheckOutSorted.java
@@ -13,16 +13,16 @@ public class ReservationByCheckOutSorted implements Comparator<Reservation> {
 	@Override
 	public int compare(Reservation r1, Reservation r2) {
 
-		if (r1.equals(r2)) {
+		if (r1.getCheckOutDate().equals(r2.getCheckOutDate())) {
 			return 0;
 		}
 
-		if (!r1.getCheckOutDate().equals(r2.getCheckOutDate())) {
-			return r1.getCheckOutDate().compareTo(r2.getCheckOutDate());
+		if (r1.getCheckOutDate().isAfter(r2.getCheckOutDate())) {
+			return 1;
 		}
 
 		// // if same customer, order by check out (i.e., default)
-		return r1.compareTo(r2);
+		return -1;
 	}
 
 }
