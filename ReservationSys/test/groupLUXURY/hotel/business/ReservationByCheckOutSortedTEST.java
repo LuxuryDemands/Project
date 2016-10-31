b05fbf620d167974b83c9af37594043ea11a26df
kajal <kajal@192.168.1.11>
2016-10-30 23:17:43 -0400

    Test class finished.

--- ReservationSys/test/groupLUXURY/hotel/business/ReservationByCheckOutSortedTEST.java
+++ ReservationSys/test/groupLUXURY/hotel/business/ReservationByCheckOutSortedTEST.java
@@ -1,9 +1,5 @@
 package groupLUXURY.hotel.business;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 /**
  * @author kajal
  *
@@ -12,40 +8,46 @@ public class ReservationByCheckOutSortedTEST {
 
 	public static void main(String[] args) {
 
-		RoomType rt1 = RoomType.PENTHOUSE;
-		DawsonRoom rom1 = new DawsonRoom(101, rt1);
+		testCompareByCheckOut();
+	}
+
+	public static void testCompareByCheckOut() {
+		
+		DawsonRoom rom1 = new DawsonRoom(101, RoomType.PENTHOUSE);
 		DawsonCustomer cus1 = new DawsonCustomer("Bruce", "Wayne", "Bruce@live.com");
-		DawsonReservation res1 = new DawsonReservation(cus1, rom1, 2017, 2, 3, 2024, 4, 7);
+		DawsonReservation res1 = new DawsonReservation(cus1, rom1, 2017, 2, 3, 2022, 4, 7);
 
-		RoomType rt2 = RoomType.NORMAL;
-		DawsonRoom rom2 = new DawsonRoom(102, rt2);
-		DawsonCustomer cus2 = new DawsonCustomer("Call", "Saul", "Call@hotmail.ca");
-		DawsonReservation res2 = new DawsonReservation(cus2, rom2, 2018, 9, 4, 2019, 6, 8);
+		DawsonRoom rom2 = new DawsonRoom(101, RoomType.PENTHOUSE);
+		DawsonCustomer cus2 = new DawsonCustomer("Bruce", "Wayne", "Bruce@live.com");
+		DawsonReservation res2 = new DawsonReservation(cus2, rom2, 2017, 2, 3, 2022, 4, 7);
 
-		RoomType rt3 = RoomType.NORMAL;
-		DawsonRoom rom3 = new DawsonRoom(103, rt3);
-		DawsonCustomer cus3 = new DawsonCustomer("Harison", "Wells", "Harison@gmail.com");
-		DawsonReservation res3 = new DawsonReservation(cus3, rom3, 2017, 7, 1, 2019, 2, 2);
+		DawsonRoom rom3 = new DawsonRoom(103, RoomType.NORMAL);
+		DawsonCustomer cus3 = new DawsonCustomer("Berry", "Allen", "Berry@yahoo.com");
+		DawsonReservation res3 = new DawsonReservation(cus3, rom3, 2017, 2, 3, 2024, 4, 7);
 
-		RoomType rt4 = RoomType.PENTHOUSE;
-		;
-		DawsonRoom rom4 = new DawsonRoom(104, rt4);
+		DawsonRoom rom4 = new DawsonRoom(104, RoomType.PENTHOUSE);
 		DawsonCustomer cus4 = new DawsonCustomer("Berry", "Allen", "Berry@yahoo.com");
-		DawsonReservation res4 = new DawsonReservation(cus4, rom4, 2019, 9, 5, 2022, 2, 8);
-
-		List<DawsonReservation> list = new ArrayList<DawsonReservation>();
-		list.add(res1);
-		list.add(res2);
-		list.add(res3);
-		list.add(res4);
-        Collections.sort(list,new ReservationByCheckOutSorted());
+		DawsonReservation res4 = new DawsonReservation(cus4, rom4, 2019, 9, 5, 2025, 2, 8);
+         
+		DawsonReservation[] list={res1,res2,res3,res4};
+		testCompareByCheckOut(list);
+	}
 	
-		for (DawsonReservation r : list) {
-			System.out.println("This is the Check out Date: " + r.getCheckOutDate());
-
-		}
-
-
+	public static void testCompareByCheckOut(DawsonReservation[]  list){
+		ReservationByCheckOutSorted reservationByCheckOutSorted = new ReservationByCheckOutSorted();
+		
+		System.out.println("Those are the test for testCompareByCheckOut class \n");
+		
+		System.out.println("Exactly the same check out date (expect 0)");
+		System.out.println(reservationByCheckOutSorted.compare(list[0],list[1]));
+	    
+		System.out.println("R1>R2 (expect a positive number)");
+		System.out.println(reservationByCheckOutSorted.compare(list[2],list[1]));
+		
+		System.out.println("R1<R2 (expect a negative number)");
+		System.out.println(reservationByCheckOutSorted.compare(list[2],list[3]));
+		
+		
 	}
 
 }
