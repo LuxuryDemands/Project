1dd4e263e674001363da5141637c9823da21a2ba
Sebastian <Sebastian@Sebastian-PC>
2016-11-23 01:18:06 -0500

    added getAvailableRoom method in DawsonHotelAllocationPolicy.java

--- ReservationSys/src/dw317/hotel/business/DawsonHotelAllocationPolicy.java
+++ ReservationSys/src/dw317/hotel/business/DawsonHotelAllocationPolicy.java
@@ -22,23 +22,29 @@ import dw317.hotel.data.interfaces.ReservationDAO;
 public class DawsonHotelAllocationPolicy implements Serializable, AllocationPolicy {
 
 	protected ReservationDAO reservDAO;
-	private static final long serialVersionUID = 42031768871L; 
+	private static final long serialVersionUID = 42031768871L;
+
+	public DawsonHotelAllocationPolicy(ReservationDAO reservDAO) {
+		this.reservDAO = reservDAO;
+	}
+
 	@Override
 	public Optional<Room> getAvailableRoom(LocalDate checkin, LocalDate checkout, RoomType type) {
 		Integer mostFrequent = 0;
 		ArrayList<Integer> floors = new ArrayList<>();
 		List<Room> availableRooms = reservDAO.getFreeRooms(checkin, checkout, type);
-		for (Room r: availableRooms){
+		for (Room r : availableRooms) {
 			floors.add(r.getFloor());
 		}
-		for (int floor = 0; floor<9;floor++){
-			if (Collections.frequency(floors, floor)>mostFrequent){
-				mostFrequent=floor;
+		for (int floor = 0; floor < 9; floor++) {
+			if (Collections.frequency(floors, floor) > mostFrequent) {
+				mostFrequent = floor;
 			}
 		}
-		for (Room r: availableRooms){
-			if (!((Integer)r.getFloor()).equals(mostFrequent)){
-				availableRooms.remove(r);
+		for (int index = 0; index < availableRooms.size(); index++) {
+			if (!((Integer) availableRooms.get(index).getFloor()).equals(mostFrequent)) {
+				availableRooms.remove(index);
+				index--;
 			}
 		}
 		Random randomizer = new Random();
